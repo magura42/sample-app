21db5104b99d3071c92e4255e133250b9b9b4c9c
Manfred Harrer <manfred.harrer@jambit.com>
2018-04-25 12:17:11 +0200

    added LB

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,13 @@ resource "azurerm_resource_group" "sample_app" {
   }
 }
 
+resource "azurerm_availability_set" "test" {
+  name                = "acceptanceTestAvailabilitySet1"
+  location            = "${azurerm_resource_group.sample_app.location}"
+  resource_group_name = "${azurerm_resource_group.sample_app.name}"
+  managed             = true
+}
+
 #Assume that custom image has been already created in the 'jambitiac' resource group
 data "azurerm_resource_group" "image" {
   name = "jambitiac"
@@ -30,7 +37,8 @@ data "azurerm_image" "image" {
 }
 
 resource "azurerm_public_ip" "test" {
-  name                         = "test_pip"
+  count                        = "${var.sample-app-count}"
+  name                         = "pip-${count.index}"
   location                     = "${azurerm_resource_group.sample_app.location}"
   resource_group_name          = "${azurerm_resource_group.sample_app.name}"
   public_ip_address_allocation = "Dynamic"
@@ -52,25 +60,48 @@ resource "azurerm_subnet" "test" {
 }
 
 resource "azurerm_network_interface" "test" {
-  name                = "acctni"
+  count               = "${var.sample-app-count}"
+  name                = "acctni-${count.index}"
   location            = "${azurerm_resource_group.sample_app.location}"
   resource_group_name = "${azurerm_resource_group.sample_app.name}"
 
   ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "static"
-    private_ip_address            = "10.0.2.5"
-    public_ip_address_id          = "${azurerm_public_ip.test.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.test.*.id[count.index]}"
+    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.test.id}"]
   }
 }
 
-resource "azurerm_virtual_machine" "test" {
-  name                  = "acctvm"
+resource "azurerm_network_security_group" "test" {
+  name                  = "nsg_test"
   location              = "${azurerm_resource_group.sample_app.location}"
   resource_group_name   = "${azurerm_resource_group.sample_app.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
+}
+
+resource "azurerm_network_security_rule" "test" {
+  name                       = "port8080"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "8080"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name        = "${azurerm_resource_group.sample_app.name}"
+  network_security_group_name = "${azurerm_network_security_group.test.name}"
+}
+
+resource "azurerm_virtual_machine" "test" {
+  count                 = "${var.sample-app-count}"
+  name                  = "acctvm-${count.index}"
+  location              = "${azurerm_resource_group.sample_app.location}"
+  resource_group_name   = "${azurerm_resource_group.sample_app.name}"
+  network_interface_ids = ["${azurerm_network_interface.test.*.id[count.index]}"]
   vm_size               = "Standard_A0"
+  availability_set_id   = "${azurerm_availability_set.test.id}"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
@@ -80,14 +111,14 @@ resource "azurerm_virtual_machine" "test" {
   }
 
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk-${count.index}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "${data.azurerm_image.image.name}"
+    computer_name  = "${data.azurerm_image.image.name}-${count.index}"
     admin_username = "jambitadmin"
     admin_password = "Password1234!"
   }
@@ -95,4 +126,53 @@ resource "azurerm_virtual_machine" "test" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+}
+
+
+
+resource "azurerm_public_ip" "lbPip" {
+  name                          = "PublicIPForLB"
+  location                      = "${azurerm_resource_group.sample_app.location}"
+  resource_group_name           = "${azurerm_resource_group.sample_app.name}"
+  public_ip_address_allocation  = "Dynamic"
+  idle_timeout_in_minutes      = 30
+}
+
+resource "azurerm_lb" "test" {
+  name                  = "lb"
+  location              = "${azurerm_resource_group.sample_app.location}"
+  resource_group_name   = "${azurerm_resource_group.sample_app.name}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.lbPip.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "test" {
+  resource_group_name = "${azurerm_resource_group.sample_app.name}"
+  loadbalancer_id     = "${azurerm_lb.test.id}"
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_rule" "test" {
+  resource_group_name            = "${azurerm_resource_group.sample_app.name}"
+  loadbalancer_id                = "${azurerm_lb.test.id}"
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 8080
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.test.id}"
+  probe_id                       = "${azurerm_lb_probe.test.id}"
+}
+
+resource "azurerm_lb_probe" "test" {
+  name                = "lb_probe-vm1"
+  resource_group_name = "${azurerm_resource_group.sample_app.name}"
+  loadbalancer_id     = "${azurerm_lb.test.id}"
+  protocol            = "tcp"
+  port                = "8080"
+  interval_in_seconds = "15"
+  number_of_probes    = "3"
 }
